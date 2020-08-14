ad7b22d4a9df48f414ed27616020e39ad5f83a19
Rudresh HM <rudreshhm2810@gmail.com>
2020-08-15 02:04:46 +0530

    commit correct info

--- src/components/Dail-section/Content/Content.js
+++ src/components/Dail-section/Content/Content.js
@@ -4,7 +4,7 @@ const Content=(props)=>{
     return(
         <div className={classes["content"]}>
             <h1 className={classes["heading-primary"]}>One step away from <span className="blue">help.</span></h1>
-            <p>How it works: Send the code in the format 0[space][code], the code is the number on the keypad corresponding to each letter of the superheroes name. </p>
+            <p>How it works: Send the code in the format 0[space][code], the code is the number on the keypad corresponding to each letter of the superhero's name. </p>
         </div>
     )
 }
